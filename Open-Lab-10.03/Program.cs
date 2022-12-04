40cc7ee11818221b682a4ddfba2d1570a54b4b04
bakelit69 <jakubchalupka385@gmail.com>
2022-12-04 21:41:07 +0100

    finally did IT :I

--- Open-Lab-10.03/Program.cs
+++ Open-Lab-10.03/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Open_Lab_10._03;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Open_Lab_10._03
     {
         static void Main(string[] args)
         {
+            Book LOTR = new Book();
+            LOTR.Title = "Lord Of The Rings";
+            LOTR.Pages = 576;
+            LOTR.Category = "Fantasy";
+            LOTR.Author = "J.R.R.Tolkien";
+            LOTR.ReleaseDate = 1954;
+            Console.WriteLine(LOTR.ToString());
         }
     }
 }
